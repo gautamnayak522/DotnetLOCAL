0d6ab88d33d11c13181a0d8dcdadfdfadb63b57c
Gautam Nayak <88781537+gautamnayak522@users.noreply.github.com>
2022-07-14 00:55:13 +0530

    day2

--- Day2/assi1/assi1/Program.cs
+++ Day2/assi1/assi1/Program.cs
@@ -6,9 +6,40 @@ namespace assi1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Person p1 = new Person("abcd", "aaa", "a@a.com","13-07-2022");
-            Console.WriteLine(p1.first_name+p1.last_name+p1.emailaddress+p1.date_of_birth+p1.Adult+p1.bday);
+            Console.WriteLine("Enter How many Person You Want to Insert ?");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Person[] persons = new Person[n];
+            string fname;
+            string lname;
+            string email;
+            DateTime dob;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"ENter Details of Student {i+1}");
+                Console.WriteLine("Enter first Name : ");
+                fname = Console.ReadLine();
+                Console.WriteLine("Enter Last Name : ");
+                lname = Console.ReadLine();
+                Console.WriteLine("Enter Email : ");
+                email = Console.ReadLine();
+                Console.WriteLine("Enter DOB : ");
+                dob = Convert.ToDateTime(Console.ReadLine());
+
+                Person p1 = new Person(fname, lname, email, dob.ToString());
+
+                persons[i] = p1;
+            }
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine($"{person.first_name}\t{person.last_name}\t{person.emailaddress}\t{person.date_of_birth}\t{person.Adult}\t{person.bday}\t{person.Sunsign}\t{person.ChineseSign}");
+            }
+
+            //Console.WriteLine("Hello World!");
+            //Person p1 = new Person("abcd", "aaa", "a@a.com","29-04-1998");
+            //Console.WriteLine(p1.first_name+p1.last_name+p1.emailaddress+p1.date_of_birth+p1.Adult+p1.bday+p1.Sunsign+p1.ChineseSign);
         }
     }
 }
